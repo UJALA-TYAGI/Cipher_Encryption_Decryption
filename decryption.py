c8e5a07f4df972dcdccafd965b3f4cc3905bc394
UJALA TYAGI <UJALATYAGI2512@GMAIL.COM>
2021-11-29 21:26:10 +0530

    Add files via upload

---
+++ decryption.py
@@ -0,0 +1,45 @@
+# The Decryption Function
+def cipher_decrypt(ciphertext, key):
+
+    decrypted = ""
+
+    for c in ciphertext:
+
+        if c.isupper():
+
+            c_index = ord(c) - ord('A')
+
+            # shift the current character to left by key positions to get its original position
+            c_og_pos = (c_index - key) % 26 + ord('A')
+
+            c_og = chr(c_og_pos)
+
+            decrypted += c_og
+
+        elif c.islower():
+
+            c_index = ord(c) - ord('a')
+
+            c_og_pos = (c_index - key) % 26 + ord('a')
+
+            c_og = chr(c_og_pos)
+
+            decrypted += c_og
+
+        elif c.isdigit():
+
+            # if it's a number,shift its actual value
+            c_og = (int(c) - key) % 10
+
+            decrypted += str(c_og)
+
+        else:
+
+            # if its neither alphabetical nor a number, just leave it like that
+            decrypted += c
+
+    return decrypted
+
+ciphertext = input("Enter cipher text: ")
+key = int(input("Enter the key: "))
+print(cipher_decrypt(ciphertext, key))
